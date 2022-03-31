a48b5c18ac0ea3975e284623423c82b3370309bd
João Pedro <joaopedro@sobremail.com>
2022-03-31 16:49:35 -0300

    params getter and multiple questions on quiz

--- commands/quiz.js
+++ commands/quiz.js
@@ -3,9 +3,10 @@ const { MessageCollector } = require('discord.js')
 module.exports = {
 
     name: 'quiz',
-    execute: async (client, message) => {
+    execute: async ({ client, message, commandParams }) => {
+        const questionNumber = commandParams.length === 0 ? 20 : Number(commandParams[0])
+        const timeForAnswers = commandParams.length > 2 ? 60 * 1000 : Number(commandParams[1]) * 1000
 
-        console.log('chamou')
         let questions = [ //it will be the API response
             { asking: 'Qual é o nome da irmã da Vi?', answer: 'Jinx' },
             { asking: 'Quantos campeões possuiam o lol em sua fase alpha?', answer: '17' },
@@ -13,15 +14,12 @@ module.exports = {
             { asking: 'Quanto tempo leva para desarmar a spike sem kit de desarme no CS:GO', answer: '10 segundos' }
         ]
 
-        //questions.forEach(async question => {
-            question = questions[Math.floor(Math.random() * questions.length)]
+        for (i = 0; i < questionNumber; i++) {
+            const question = questions[Math.floor(Math.random() * questions.length)]
             await message.channel.send(question.asking)
-            //const collector = new MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 })
-            //console.log(collector)
-            let filter = m => m.author.id === message.author.id
-            await message.channel.awaitMessages({filter, max: 1, time: 60000, errors: ['time'] })
+            await message.channel.awaitMessages({ max: 1, time: timeForAnswers, errors: ['time'] })
                 .then(message => {
-                    console.log('durante a promisse')
+                    console.log('mensagem recebida: ', message)
                     message = message.first()
                     if (message.content === question.answer) {
                         message.channel.send('acertou :)')
@@ -30,10 +28,10 @@ module.exports = {
                     }
                 })
                 .catch(collected => {
-                    message.channel.send('Timeout');
+                    message.channel.send('Ninguéeeeeeeem acertou');
                 });
             console.log('esperou a promisse')
-        //})
+        }
 
     }
 
