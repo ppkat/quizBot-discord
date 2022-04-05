03959952dc1fc0831a1a1ad21d3b6869dc110fbe
João Pedro <joaopedro@sobremail.com>
2022-04-05 12:05:51 -0300

    commands complete

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,17 +1,14 @@
+const { registeredUsers } = require('../commands/quiz')
 const quiz = require('../commands/quiz')
 
 module.exports = {
     name: 'interactionCreate',
     once: false,
-    listen: async (client, interaction) => { 
-        if(!interaction.isButton()) return
+    listen: async (client, interaction) => {
+        if (!interaction.isButton()) return
 
-        if(interaction.customId === 'entrar'){
-            console.log('entrou aqui')
-            interaction.reply({ content: 'Você foi inscrito no quiz! Boa sorte!', ephemeral: true })
-
-            quiz.registeredUsers.push(interaction.member.id)
-            quiz.updateRegisteredUsers()
+        if (interaction.customId === 'entrar') {
+            quiz.updateRegisteredUsers(interaction)
         }
     }
 }
