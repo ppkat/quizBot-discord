c510819320b6727eac1e9d3ac54a4fa37c936fa3
Igor Jacques <igorjcqs@gmail.com>
2022-04-17 16:21:12 -0300

    feat: sequelize setup

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
-const { Sequelize, DataTypes } = require('sequelize')
-require('dotenv').config()
+const { Sequelize, DataTypes } = require("sequelize");
+require("dotenv").config();
 
 const sequelize = new Sequelize(
   process.env.DATABASE_NAME,
@@ -13,101 +13,103 @@ const sequelize = new Sequelize(
 
 // const sequelize = new Sequelize('pbdb', 'root', '0/6?Szgo)JFf*98@:e', { host: 'localhost', dialect: 'mysql' })
 
-sequelize.authenticate().then(() => console.log('Entrou no banco de dados')).catch(err => console.log(err))
+sequelize
+  .authenticate()
+  .then(() => console.log("Entrou no banco de dados"))
+  .catch((err) => console.log(err));
 
-const Participant = sequelize.define('Participants', {
+const Participant = sequelize.define("Participants", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+    primaryKey: true,
+  },
 
-    id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        allowNull: false,
-        primaryKey: true
-    },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
 
-    username: {
-        type: DataTypes.STRING,
-        allowNull: true
-    },
+  userId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 
-    userId: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
+  score: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
 
-    score: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    }
+const Reward = sequelize.define("Rewards", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+    primaryKey: true,
+  },
 
-})
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 
-const Reward = sequelize.define('Rewards', {
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
 
-    id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        allowNull: false,
-        primaryKey: true
-    },
+  redeemed: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+    allowNull: false,
+  },
 
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
+  rewardCode: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
 
-    description: {
-        type: DataTypes.TEXT,
-        allowNull: false
-    },
-
-    redeemed: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false,
-        allowNull: false
-    },
-
-    rewardCode: {
-        type: DataTypes.TEXT,
-        allowNull: false
-    },
-
-    winnerDiscordId: {
-        type: DataTypes.TEXT
-    }
-
-})
+  winnerDiscordId: {
+    type: DataTypes.TEXT,
+  },
+});
 
 // sequelize.sync({force: true}) //add the models on database generating tables
 
-
 async function updateRank() {
+  const allUsers = await Participant.findAll();
+  let usersNotRemoved = allUsers.map((users) => users.dataValues);
 
-    const allUsers = await Participant.findAll()
-    let usersNotRemoved = allUsers.map(users => users.dataValues)
+  let scores = usersNotRemoved.map((user) => user.score);
+  let updatedRanks = [];
+  for (i = 0; i < allUsers.length; i++) {
+    const biggerScoreIndex = scores.findIndex(
+      (score) => score === Math.max(...scores)
+    );
 
-    let scores = usersNotRemoved.map(user => user.score)
-    let updatedRanks = []
-    for (i = 0; i < allUsers.length; i++) {
-        const biggerScoreIndex = scores.findIndex(score => score === Math.max(...scores))
+    updatedRanks.push(
+      usersNotRemoved.find((user) => user.score === Math.max(...scores))
+    );
+    scores.splice(biggerScoreIndex, 1);
+    usersNotRemoved.splice(biggerScoreIndex, 1);
+  }
 
-        updatedRanks.push(usersNotRemoved.find(user => user.score === Math.max(...scores)))
-        scores.splice(biggerScoreIndex, 1)
-        usersNotRemoved.splice(biggerScoreIndex, 1)
-    }
-
-    return updatedRanks
+  return updatedRanks;
 }
 
 async function getNoRedeemedRewards() {
+  const allRewards = (await Reward.findAll()).map((reward) => reward);
+  const noRedeemedRewards = allRewards.filter(
+    (reward) => !reward.dataValues.redeemed
+  );
 
-    const allRewards = (await Reward.findAll()).map(reward => reward)
-    const noRedeemedRewards = allRewards.filter(reward => !reward.dataValues.redeemed)
-
-    return noRedeemedRewards
+  return noRedeemedRewards;
 }
 
 module.exports = {
-    ParticipantDB: Participant,
-    updateRank,
-    getNoRedeemedRewards
-}
+  ParticipantDB: Participant,
+  updateRank,
+  getNoRedeemedRewards,
+};
---
+++ migrations/20220417191006-create_all_tables.js
@@ -0,0 +1,68 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Participants", {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      userId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      score: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+
+    await queryInterface.createTable("Rewards", {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      redeemed: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      rewardCode: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      winnerDiscordId: {
+        type: Sequelize.TEXT,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Participants");
+    await queryInterface.dropTable("Rewards");
+  },
+};
